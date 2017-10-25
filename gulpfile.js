242d718b8adcb2b0631ceb5684787b39935589e9
dvcone <dani2014riau@gmail.com>
2017-10-25 14:48:09 +0700

    aoa aoa

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+
+var browserSync = require('browser-sync').create();
+
+  gulp.task('sass', function(){
+    return gulp.src('source-files')
+      .pipe(sass().on('error', sass.logError))// Using gulp-sass
+      .pipe(gulp.dest('destination'))
+  });
+
+  gulp.task('sass', function(){
+    return gulp.src('app/scss/**/*scss')
+      .pipe(sass()) // Converts Sass to CSS with gulp-sass
+      .pipe(gulp.dest('app/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+  });
+
+  gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: 'app'
+      },
+    })
+  })
+
+  gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
+  
+
+
